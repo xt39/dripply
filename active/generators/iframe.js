00d702424263301847f075b38c3b38842c660157
Maysun Campos <maysuncampos@yahoo.com>
2023-03-30 14:35:16 -0600

    IFrame code generator

---
+++ active/generators/iframe.js
@@ -0,0 +1,6 @@
+"use strict";
+
+window.onload = function() {
+  document.querySelector('.frame').innerHTML = "<iframe src='' frameborder='0'></iframe>";
+  document.querySelector('#frame').innerHTML = "<iframe src='' frameborder='0'></iframe>";
+}
