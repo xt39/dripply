96f1160f41427491c166ee30fcb988d37f035ef2
Maysun Campos <maysuncampos@yahoo.com>
2023-03-30 14:54:32 -0600

    font selecting

---
+++ actions/font/fontSelector.js
@@ -0,0 +1,11 @@
+"use strict";
+
+var font = "";
+
+if (font == "serif") {
+  document.body.style = "font-family:serif;";
+} else if (font == "sanserif") {
+  document.body.style = "font-family:sans-serif;";
+} else {
+  // do nothing
+}
