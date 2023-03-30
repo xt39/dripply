85022682253be49fa68761b120f809d0fce4061f
Maysun Campos <maysuncampos@yahoo.com>
2023-03-30 14:43:12 -0600

    Copyright year updating automatically and it generates the code

---
+++ active/generators/copyright_year_update.js
@@ -0,0 +1,8 @@
+"use strict";
+
+window.onload = function() {
+  var d = new Date();
+  var y = d.getFullYear();
+  document.querySelector(".year").innerHTML = y;
+  document.querySelector("#year").innerHTML = y;
+}
