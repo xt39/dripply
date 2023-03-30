8a9fb6724a0696efd316b00eddf94e439983b52a
Maysun Campos <maysuncampos@yahoo.com>
2023-03-30 14:49:06 -0600

    Theme changer

---
+++ actions/scheme/themes.js
@@ -0,0 +1,9 @@
+"use strict";
+
+function dark() {
+  document.body.style = "color:white; background:rgb(18, 18, 18);";
+}
+
+function light() {
+  document.body.style = "color:black; background:white;";
+}
