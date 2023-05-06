5e3188c94c610ac249e3c16cebe1cd18f1919e17
Maysun Campos <maysuncampos@yahoo.com>
2023-05-06 11:02:34 -0600

    Add some more code and clean up some things

---
+++ active/tools/changers.js
@@ -0,0 +1,9 @@
+"use strict";
+
+// make shortening varaibles
+
+var doc, win, say;
+
+doc = document;
+win = window;
+say = alert;
---
+++ active/tools/operators.js
@@ -0,0 +1,13 @@
+"use strict";
+
+// base ten
+var one, two, three, four, five, six, seven, eight, nine, ten;
+
+// operating handlers
+var times, numA, numB, divi, numDA, numDB;
+var numPA, numPB, plus, minus, numMA, numMB;
+
+times = numA * numB;
+divi = numDA / numDB;
+plus = numPA + numPB;
+minus = numMA - numMB;
